20d5478dada564523826d188fa412c35e36e7f59
Samuel Bernheim <sambernheim@gmail.com>
2017-01-09 18:49:23 -0500

    Add babel task for js

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,21 @@ const cleanCSS = require('gulp-clean-css');
 
 gulp.task('minifyCSS', function() {
 	return gulp.src('css/*.css')
-	.pipe(cleanCSS())
-	.pipe(gulp.dest('minifiedCSS'));
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('minifiedCSS'));
 });
 
+gulp.task('buildJS', function() {
+	return gulp.src('js/*.js')
+		.pipe(babel())
+		.pipe(gulp.dest('compiledJS'));
+})
 
 // watch all css files for changes
 gulp.task('watch', function() {
-  gulp.watch('css/*.css', ['minifyCSS']);
+	gulp.watch('css/*.css', ['minifyCSS']);
+	gulp.watch('js/*.js', ['compiledJS']);
+
 });
 
 // The default task (called when you run `gulp` from cli)
