14ff9619ad9227d4c6536517df6256508f902d8c
Samuel Bernheim <sambernheim@gmail.com>
2017-01-09 22:46:40 -0500

    Add JSHint + Mobile navbar

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,23 @@ const cleanCSS = require('gulp-clean-css');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const sass = require('gulp-sass');
+const jshint = require('gulp-jshint');
 
 gulp.task('buildCSS', function() {
 	return gulp.src('scss/*.css')
-		.pipe(sass())
-		.pipe(cleanCSS())
-		.pipe(concat('index.css'))
-		.pipe(gulp.dest('server/public'));
+	.pipe(sass())
+	.pipe(cleanCSS())
+	.pipe(concat('index.css'))
+	.pipe(gulp.dest('server/public'));
 });
 
 gulp.task('buildJS', function() {
 	return gulp.src('js/*.js')
-		.pipe(babel())
-		.pipe(concat('index.js'))
-		.pipe(gulp.dest('server/public'));
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'))
+	.pipe(babel())
+	.pipe(concat('index.js'))
+	.pipe(gulp.dest('server/public'));
 })
 
 
@@ -28,4 +31,5 @@ gulp.task('watch', function() {
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch']);
+// first run buildCSS, then buildJS, and then start watching
+gulp.task('default', ['buildCSS', 'buildJS', 'watch']);
