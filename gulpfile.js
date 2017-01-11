6a6064bb9fbe3d488f9ad3b2df72ea5ec1fef0a1
Samuel Bernheim <sambernheim@gmail.com>
2017-01-11 01:06:49 -0500

    Minify html with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,30 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const sass = require('gulp-sass');
 const jshint = require('gulp-jshint');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('buildHTML', function() {
+	return gulp.src('html/*.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('minihtml'));
+});
+
 
 gulp.task('buildCSS', function() {
 	return gulp.src('scss/main.scss')
-	.pipe(sass())
-	.pipe(cleanCSS())
-	.pipe(concat('index.css'))
-	.pipe(gulp.dest('server/public'));
+		.pipe(sass())
+		.pipe(cleanCSS())
+		.pipe(concat('index.css'))
+		.pipe(gulp.dest('server/public'));
 });
 
 gulp.task('buildJS', function() {
 	return gulp.src('js/*.js')
-	.pipe(jshint())
-	.pipe(jshint.reporter('default'))
-	.pipe(babel())
-	.pipe(concat('index.js'))
-	.pipe(gulp.dest('server/public'));
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(babel())
+		.pipe(concat('index.js'))
+		.pipe(gulp.dest('server/public'));
 })
 
 
@@ -27,9 +35,9 @@ gulp.task('buildJS', function() {
 gulp.task('watch', function() {
 	gulp.watch('scss/*.scss', ['buildCSS']);
 	gulp.watch('js/*.js', ['buildJS']);
-
+	gulp.watch('html/*.html', ['buildHTML']);
 });
 
 // The default task (called when you run `gulp` from cli)
 // first run buildCSS, then buildJS, and then start watching
-gulp.task('default', ['buildCSS', 'buildJS', 'watch']);
+gulp.task('default', ['buildCSS', 'buildJS', 'buildHTML','watch']);
--- server/index.js
+++ server/index.js
@@ -11,11 +11,11 @@ app.use(express.static(path.join(__dirname, 'public'))); //css
 app.use(express.static(path.join(__dirname, '../images'))); // images
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, '../html/home.html'));
+	res.sendFile(path.join(__dirname, '../minihtml/home.html'));
 });
 
 app.get('/resume', function(req, res) {
-	res.sendFile(path.join(__dirname, '../html/resume.html'));
+	res.sendFile(path.join(__dirname, '../minihtml/resume.html'));
 });
 
 // listen on port
