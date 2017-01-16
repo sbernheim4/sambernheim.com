7d70b62b2b6ff436fe0fc40d86a23828a0aa3add
Samuel Bernheim <sambernheim@gmail.com>
2017-01-16 16:57:27 -0500

    Remove old comments

--- server/index.js
+++ server/index.js
@@ -1,17 +1,15 @@
-'use strict'
+'use strict';
 
 var express = require('express');
 var app = express();
 var path = require('path');
 
 var port = process.env.PORT || 1337;
-var oneDay = 86400000;
-
-// enable g-zip compression
+var cacheTime = 86400000;
 
 // serve static files
-app.use(express.static(path.join(__dirname, 'public'), {maxAge: oneDay})); //css
-app.use(express.static(path.join(__dirname, '../images'), {maxAge: oneDay})); // images
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: cacheTime})); //css
+app.use(express.static(path.join(__dirname, '../images'), {maxAge: cacheTime})); //images
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '../minihtml/home.html'));
