ac7ad0143affebd321b84c5b47273a339f6be87d
sbernheim4 <sambernheim@gmail.com>
2017-08-23 17:23:02 -0400

    Add bash script to get website running on AWS ec2 instance

---
+++ aws-set-up.sh
@@ -0,0 +1,28 @@
+# Install nvm and use nvm to install latest version of node/npm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install node
+nvm use node
+
+# Install git, clone repo, install packages
+sudo yum install -y git
+git clone https://github.com/sbernheim4/Personal-Website.git
+cd Personal-Website
+npm install
+
+# Set up port forwarding. Redirect all incoming port 80 requests to port 8080
+sudo iptables --insert INPUT --protocol tcp --dport 80 --jump ACCEPT
+sudo iptables --insert INPUT --protocol tcp --dport 8080 --jump ACCEPT
+sudo iptables --table nat --append PREROUTING --in-interface eth0 --protocol tcp --dport 80 --jump REDIRECT --to-port 8080
+# run next line to have changes survive reboot
+sudo service iptables save
+
+# Set environment variable PORT to be 8080
+export PORT=8080
+
+# Start the application
+npm start
