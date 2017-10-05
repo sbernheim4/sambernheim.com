ae2e9e58d9091a9407c0dcc53e782a9498bbd218
sbernheim <sambernheim@gmail.com>
2017-10-04 21:39:07 -0400

    Syntax cleanup

--- server/blog.js
+++ server/blog.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 const router = express.Router();
@@ -10,27 +10,39 @@ var bodyParser = require('body-parser');
 const url = process.env.DB_URI;
 
 MongoClient.connect(url, (err, database) => {
-	if (err) {
-		console.log(err);
-	} else {
-		db = database;
-		if (err) throw err;
-		router.get('/', (req, res) => {
-			db.collection('blog').find({}).toArray(function (err, result) {
-				const html = generateHTML(result)
-				res.send(html);
-			});
+	if (err) throw err;
+	db = database;
+	router.get('/', (req, res) => {
+		db.collection('blog').find({}).toArray( (err, result) => {
+			if (err) throw err;
+			const html = generateHTML(result)
+			res.send(html);
 		});
-	}
+	});
 });
 
 
 function generateHTML(records) {
-	// Beginning of HTML response that will be sent
+	records = records.reverse();
 	let html = `
 		<!DOCTYPE html>
 		<html>
 			<head>
+				<meta charset="UTF-8" />
+				<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+				<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+				<meta name="theme-color" content="#3c3c3c">
+				<title>Samuel Bernheim</title>
+
+				<!-- My Icon -->
+				<link rel="icon" href="icon144x144.png">
+
+				<!-- My Manifest.json -->
+				<link rel="manifest" href="manifest.json">
+
+				<!-- Google Analytics tracking code -->
+				<meta name="google-site-verification" content="zHcVBbY7cgsbEuhwVpBJXUmxIoOJVjyx7OFqxmUequM" />
 				<title>Welcome to my Blog</title>
 				<link rel="stylesheet" href="index.css">
 			</head>
--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-require('dotenv').config()
-
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -11,29 +9,26 @@ const chalk = require('chalk');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(compression());
 
 const port = process.env.PORT || 1337;
 const cacheTime = 31536000000; // One year
 
-const MongoClient = require('mongodb').MongoClient
-
-app.use(compression());
-
 // serve static files
 app.use(express.static(path.join(__dirname, 'public'), {maxAge: cacheTime})); //css and js
 app.use(express.static(path.join(__dirname, '../images'), {maxAge: cacheTime})); //images
-app.use(express.static(path.join(__dirname, '../js'), {maxAge: cacheTime}));
+app.use(express.static(path.join(__dirname, '../js'), {maxAge: cacheTime})); // used for /submit-article
 
 app.all('*', (req, res, next) => {
 	console.log(chalk.blue(`New ${req.method} request for ${req.path}`));
 	next();
 });
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../minihtml/home.html'));
 });
 
-app.get('/resume', function(req, res) {
+app.get('/resume', (req, res) => {
 	res.sendFile(path.join(__dirname, '../minihtml/resume.html'));
 });
 
@@ -44,32 +39,18 @@ const blog = require('./blog');
 app.use('/blog', blog);
 
 
-app.get('/robots.txt', function(req, res) {
+app.get('/robots.txt', (req, res) => {
 	res.sendFile(path.join(__dirname, 'robots.txt'));
 });
 
-app.get('/sitemap.xml', function(req, res) {
+app.get('/sitemap.xml', (req, res) => {
 	res.sendFile(path.join(__dirname, 'sitemap.xml'));
 });
 
-app.get('/manifest.json', function(req, res) {
+app.get('/manifest.json', (req, res) => {
 	res.sendFile(path.join(__dirname, '../manifest.json'));
 });
 
-let db;
-const url = process.env.DB_URI;
-
-MongoClient.connect(url, (err, database) => {
-	if (err) {
-		console.log(err);
-	} else {
-		db = database;
-
-		// listen on port
-		app.listen(port, function() {
-			console.log('Listening to port', port);
-		});
-	}
-
+app.listen(port, () => {
+	console.log(chalk.yellow('Listening to port:', port));
 });
-
