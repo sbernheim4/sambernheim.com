9f473ad7f46d0bd658b91fd014084e307d9e04aa
sbernheim4 <sambernheim@gmail.com>
2021-08-30 17:33:17 -0400

    feat: navbar width adjusts to progress

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,15 +1,40 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from 'react-router-dom';
+import { useProgress } from './../hooks/useProgress.js';
 
-import "./navbar.css";
+import "./navbar.scss";
 
 export default function Navbar() {
+
+	const { pathname } = useLocation();
+	const progress = useProgress();
+	const [ overrideStyle, setOverrideStyle ] = useState({});
+
+	useEffect(() => {
+
+		if (pathname.includes("blog/")) {
+
+			const maximumNavbarLineWidth = .6;
+
+			setOverrideStyle({
+				width: progress * maximumNavbarLineWidth * 100 + "%"
+			});
+
+		} else {
+
+			setOverrideStyle({});
+
+		}
+
+	}, [progress, pathname]);
+
+	console.log(overrideStyle);
 
 	return (
 		<nav className='navbar'>
 			<Link id="main" to='/'>Home</Link>
 
-			<hr/>
+			<hr style={overrideStyle}/>
 
 			<Link to='/blog'>Blog</Link>
 		</nav>
