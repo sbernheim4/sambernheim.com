338aa66cd754df48071f8ae5b3b93fe2c8a7caef
sbernheim4 <sambernheim@gmail.com>
2021-08-29 22:33:04 -0400

    remove console.log

--- src/Routes/Blog/Articles/EngineersSchrodingersCat.jsx
+++ src/Routes/Blog/Articles/EngineersSchrodingersCat.jsx
@@ -197,7 +197,6 @@ export const EngineersSchrodingersCat = () => {
 		components={{
 			code({node, inline, className, children, ...props}) {
 				const match = /language-(\w+)/.exec(className || '');
-				console.log(match)
 
 				return !inline && match ?
 					(
