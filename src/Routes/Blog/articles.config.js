cce85bfdbaa46dcbe0d4f81a09327a6ab2e36142
sbernheim4 <sambernheim@gmail.com>
2021-09-20 19:18:19 -0400

    article: begin work on next article

--- src/Routes/Blog/articles.config.js
+++ src/Routes/Blog/articles.config.js
@@ -1,7 +1,14 @@
 import { EngineersSchrodingersCat } from './Articles/EngineersSchrodingersCat.jsx';
 import { MonadsAreMonoidsInTheCategoryOfEndofunctors } from './Articles/MonadsAreMonoidsInTheCategoryOfEndofunctors.jsx';
+import { BuildingAMonad } from './Articles/BuildingAMonad.jsx';
 
 export const articles = [
+	{
+		title: "Building a Monad",
+		to: "building-a-monad",
+		Component: BuildingAMonad,
+		id: 3
+	},
 	{
 		title: "Monads are Monoids in the Category of Endofunctors",
 		to: "monads-are-monoids-in-the-category-of-endofunctors",
