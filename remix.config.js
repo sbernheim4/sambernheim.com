6533fad13177555325ddf504dfa989c34e35fd8e
sbernheim4 <sambernheim@gmail.com>
2021-12-24 16:38:04 -0500

    feat: use cloudflare pages

--- remix.config.js
+++ remix.config.js
@@ -3,9 +3,11 @@
  */
 module.exports = {
   appDirectory: "app",
-  browserBuildDirectory: "public/build",
+  assetsBuildDirectory: "public/build",
   publicPath: "/build/",
+  serverModuleFormat: "esm",
+  serverPlatform: "neutral",
   serverBuildDirectory: "build",
   devServerBroadcastDelay: 1000,
-  devServerPort: 8002
+  ignoredRouteFiles: [".*"]
 };
